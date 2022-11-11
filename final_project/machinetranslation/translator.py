3f0ee0554778cd47b7b761cf123a37e2c153e966
Gislain <gislainmba@yahoo.fr>
2022-11-11 13:54:55 -0500

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,6 +9,22 @@ load_dotenv()
 apikey = os.environ['rzQU50_N2tufIr97-AT3ktZtfHrQHdWWAZjaFBy9JDZP']
 url = os.environ['https://api.us-south.language-translator.watson.cloud.ibm.com/instances/e384ba1e-7e98-4a4f-af72-26183562720c']
 
-translator= Translator(from_lang="english",to_lang="french")
-translation = translator.translate("Guten Morgen")
-print translation
+authenticator = IAMAuthenticator('apikey')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('url')
+
+def englishToFrench(englishText):
+    frenchText = language_translator.translate(
+    text = englishText,
+    model_id='en-fr').get_result()
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    englishText = language_translator.translate(
+    text = frenchText,
+    model_id='fr-en').get_result()
+    return englishText
