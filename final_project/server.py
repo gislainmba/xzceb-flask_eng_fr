1351768c3f069108efe77b670d3b5ab447414194
Gislain <gislainmba@yahoo.fr>
2022-11-15 06:07:03 -0500

    final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,4 +1,3 @@
-import machinetranslation
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
@@ -19,7 +18,7 @@ def frenchToEnglish():
 
 @app.route("/")
 def renderIndexPage():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
